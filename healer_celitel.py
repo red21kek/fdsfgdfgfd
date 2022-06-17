f5f88c2fa3746145ddec46cccebd256b1c2186ac
red21kek <106625468+red21kek@users.noreply.github.com>
2022-06-17 09:03:22 -0400

    Update healer_celitel.py

--- healer_celitel.py
+++ healer_celitel.py
@@ -8,21 +8,40 @@ from collections import defaultdict
 
 app = Client("my_account")
 
-@app.on_message(filters.regex('я за целителя') & filters.me)
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+import time
+from typing import Union
+from time import sleep
+import random
+from time import sleep
+from collections import defaultdict
+from subprocess import check_output
+from pyrogram.types import InputMediaPhoto, InputMediaVideo, InputMediaAudio
+app = Client("my_account")
+
+@app.on_message(filters.regex('за масон') & filters.me)
 def ghoul_spam_handler(client, message):
     i = 10000
     while i > 0:
         try:
-            #привет если хочется кинуть гифку надо будет подумать
-            #app.send_video(" тут id чата скину во время рейда ", "твоя гифка которую надо скачать))")
-            #крч тут тоже самое
-            #app.send_sticker("id тоже кину", "@idstickerbot боту кинешь стикер и н тебе отправит ")
-            app.send_video("-1001715091530", "85GG.gif")
-            app.send_sticker("-1001715091530", "CAACAgQAAxkBAAEFBVxipjnGczEMrvxfIvx8KWew79C-5wAC3QkAAjANOVOxYze4AAFC2rckBA")
-            client.send_message(message.chat.id, f'привет от целителя \n привет от целителя')
+            
+            client.send_message(message.chat.id, f'Слава Отцу! DEUS VULT! AVE MARIA! СМЕРТЬ СОДОМИТАМ! \n Слава Отцу! DEUS VULT! AVE MARIA! СМЕРТЬ СОДОМИТАМ!')
+            app.send_sticker("-1001555594476", "CAACAgIAAxkBAAEFETtirHozEZGRXugNXBnlRx4mqf04OgACTBMAAmkm6EvdZsG93QtzZiQE")
         except FloodWait as e:
             sleep(e.x)
 
         i -= 7
-        sleep(0.05) 
+        sleep(0.05)           
+@app.on_message(filters.regex('цел') & filters.me)
+def ghoul_spam_handler(client, message):
+    i = 10000
+    while i > 0:
+        try:
+            app.send_animation("-1001555594476", "85GG.gif", unsave=True)
+        except FloodWait as e:
+            sleep(e.x)
+
+        i -= 7
+        sleep(0.05)
 app.run()
